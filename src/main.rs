bdf1ab9bc55b3b01ece3853bd4d1a5bd3493b414
ironhokage <106948262+ironhokage@users.noreply.github.com>
2025-12-02 11:27:30 +0200

    Adding the ability to detectthe output device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rodio::cpal::traits::{DeviceTrait, HostTrait};
+use rodio::cpal;
+
 fn main() {
-    println!("Hello, world!");
+    match get_name_of_audio_device() {
+        Some(name) => println!("Using audio device: {}", name),
+        None => println!("No default output device found, or name could not be retrieved."),
+    }
+}
+fn get_name_of_audio_device() -> Option<String> {
+    // Get the default audio host
+    let host = cpal::default_host();
+    let device_name = host.default_output_device()?; // The '?' operator propagates the None/Error
+
+    // Try to get the device's name and return it wrapped in Some()
+    // We use ? here as well to handle potential errors from device.name()
+    match device_name.name() {
+        Ok(name) => Some(name),
+        Err(e) => {
+            eprintln!("Error retrieving device name: {}", e);
+            None // Return None if we can't get the name string
+        }
+    }
 }
